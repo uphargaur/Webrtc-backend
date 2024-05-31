a3db040c4efc991830b7a986ca1bd125f84081fd
Tushar <tushargarg0987@gmail.com>
2024-05-31 15:22:13 +0530

    added: port number

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def handle_message(message):
             }), room=user_to_receive_ice_candidate['conn'])
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0',port=8080, debug=True)
